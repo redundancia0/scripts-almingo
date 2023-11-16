fe7935f67b6fd2f0505ff8bf34da1e459126e11d
redundancia0 <149798201+redundancia0@users.noreply.github.com>
2023-11-16 13:09:32 +0100

    Mejorando el código de InstalarProgramas.bat

--- instalarProgramas.bat
+++ instalarProgramas.bat
@@ -2,12 +2,16 @@
 
 @REM Instalando todos los programas mediante el uso de winget
 
+echo Instalando Google Chrome
 winget install -e --id Google.Chrome
 
+echo Instalando Visual Studio Code
 winget install -e --id Microsoft.VisualStudioCode
 
+echo Instalando Git
 winget install -e --id Git.Git
 
+echo Instalando Java JDK
 winget install -e --id Oracle.JDK.17
 
 echo Se han instalado todos los programas
